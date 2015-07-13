192ecab8389c7bdc7d01b7a1c517e73f076a4ee7
Aine de Leastar <adeleastar@outlook.com>
2015-07-13 10:35:02 +0100

    story_09 FINISHED 130715

--- public/javascripts/report.js
+++ public/javascripts/report.js
@@ -7,15 +7,22 @@ $('.ui.form')
 	'email': {
 		identifier  : 'email',
 		rules: [{
-		        	type   : 'empty',
-		        	prompt : 'Please select a Candidate'
-		        }]
+			type   : 'empty',
+			prompt : 'Please select a Candidate'
+		}]
 	},
 	'donorEmail': {
 		identifier  : 'donorEmail',
 		rules: [{
-		        	type   : 'empty',
-		        	prompt : 'Please select a Donor'
-		        }]
+			type   : 'empty',
+			prompt : 'Please select a Donor'
+		}]
+	},
+	'state': {
+		identifier  : 'state',
+		rules: [{
+			type   : 'empty',
+			prompt : 'Please select a State'
+		}]
 	}
 });
