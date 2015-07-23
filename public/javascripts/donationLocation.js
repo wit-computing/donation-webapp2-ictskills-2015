538bf127dc18a887de0cacc4b6aa1e8025561261
Aine de Leastar <adeleastar@outlook.com>
2015-07-23 09:30:16 +0100

    story_15

--- public/javascripts/donationLocation.js
+++ public/javascripts/donationLocation.js
@@ -1,55 +1,63 @@
-var map; 
-var latlng = []; 
-var markers = []; 
+var map; // the google map
+var latlng = []; // geolocation data later retrieved from server in func callback
+var markers = []; // array of all markers (unfiltered)
 var startAllowed; // boolean to enforce start() invocation once only between refreshes
 var pos = []; // array of lat, lng representing the polyline start and endpoints created by clicking map
 var posIndex = 0; // index variable associate with pos[]
 
-
+/**
+ * Render the basic google map
+ */
 function initialize() {
-	rendermap();
-	retrieveMarkerLocations();
+  rendermap();
+  retrieveMarkerLocations();
 }
 
-
+/**
+ * The basic map, no markers, no centre specified
+ * Canvas id on html is 'googleMap'
+ */
 function rendermap() {
-	var mapProp = {
-			mapTypeId:google.maps.MapTypeId.ROADMAP
-	};
-	map = new google.maps.Map($("#googleMap")[0], mapProp); 
+  var mapProp = {
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  //map = new google.maps.Map(document.getElementById("googleMap"), mapProp); // using vanilla js
+  map = new google.maps.Map($("#googleMap")[0], mapProp); // using jQuery
 }
 
 /**
  * Use ajax call to get users and their geolocations
  * pass returned array marker locations to callback method
  * Here is the format in which marker data stored
- * geoObj[0] is user.             
+ * geoObj[0] is descripion.             
  * geoObj[1] is latitude                              
  * geoObj[2] is longitude  
- **/
+ * We use geoObj[0] in the infoWindow. Click marker to reveal description.
+ */
 function retrieveMarkerLocations()
 {
-	$(function() {
-		$.get("/donationLocation/geolocations", function(data) {
-			$.each(data, function(index, geoObj) {
-				console.log(geoObj[0] + " " + geoObj[1] + " " + geoObj[2]);
-			});
-			callback(data);
-		});
-	});
+  $(function() {
+    $.get("/donationLocation/geolocations", function(data) {
+      $.each(data, function(index, geoObj) {
+        console.log(geoObj[0] + " " + geoObj[1] + " " + geoObj[2]);
+    });
+      callback(data);
+    });
+  });
 }
 
 /**
- * Put data into an array using format is 'user, xx.xxxx, yy.yyyyy' -> 
- * (user, lat, lng)  * then invoke 'fitBounds' to render the markers, 
- * centre map and create infoWindow to display firstName
+ * we've got the marker location from data in ajax call
+ * we now put data into an array
+ * the format is 'firstName, xx.xxxx, yy.yyyyy' -> (firstName, lat, lng)
+ * then invoke 'fitBounds' to render the markers, centre map and create infoWindow to display firstName
  */
-function callback(data) {
+function callback(data)
+{
 
-	latlng = data; // store the array of data in a global for later use
-	fitBounds(latlng); // then invoke fitBounds to zoom and display markers
-	setInfoWindowListener(latlng);
-	populateTable();
+  latlng = data; // store the array of data in a global for later use
+  fitBounds(latlng); // then invoke fitBounds to zoom and display markers within view
+  setInfoWindowListener(latlng);
 }
 
 /**
@@ -58,46 +66,51 @@ function callback(data) {
  */
 function fitBounds(latlngStr)
 {
-	var bounds = new google.maps.LatLngBounds();
-	for (i = 0; i < latlngStr.length; i++) 
-	{
-		marker = new google.maps.Marker({
-			position: getLatLng(latlngStr[i]),
-			map: map
-		});
-		markers[i] = marker;      
-		bounds.extend(marker.position);
-	}
-	map.fitBounds(bounds);
+  var bounds = new google.maps.LatLngBounds();
+    for (i = 0; i < latlngStr.length; i++) 
+    {
+        marker = new google.maps.Marker({
+            position: getLatLng(latlngStr[i]),
+            map: map
+        });
+        markers[i] = marker;      
+        bounds.extend(marker.position);
+    }
+    map.fitBounds(bounds);
 }
 
 function setInfoWindowListener(latlngStr)
 {
-	var infowindow = new google.maps.InfoWindow();
-	for (i = 0; i < latlng.length; i++) 
-	{
-		/*respond to click on marker by displaying infowindow text*/
-		var marker = markers[i];
-		google.maps.event.addListener(marker, 'click', (function (marker, i) {
-			return function () {
-				infowindow.setContent(latlngStr[i][0]);
-				infowindow.open(map, marker);
-			}
-		})(marker, i));
-	}
+    var infowindow = new google.maps.InfoWindow();
+    for (i = 0; i < latlng.length; i++) 
+    {
+      /*respond to click on marker by displaying infowindow text*/
+      var marker = markers[i];
+      google.maps.event.addListener(marker, 'click', (function (marker, i) {
+          return function () {
+            infowindow.setContent(latlngStr[i][0]);
+            infowindow.open(map, marker);
+          }
+      })(marker, i));
+    }
 }
-
 /**
  * A helper function to convert the latlng string to individual numbers
  * and thence to a google.maps.LatLng object
- * @param str str is list of strings : username, lat, lon                               
+ * @param str str is list of strings : username, lat, lon    
+ * str[0] is description                
+ * str[1] is latitude                              
+ * str[2] is longitude                             
+ * 
+ * @param The object 'str' holding an individual marker data set
+ * @return A google.maps.LatLng object containing the marker coordinates.
  */
 function getLatLng(str)
 { 
 
-	var lat = Number(str[1]);
-	var lon = Number(str[2]);
-	return new google.maps.LatLng(lat, lon);
+  var lat = Number(str[1]);
+  var lon = Number(str[2]);
+  return new google.maps.LatLng(lat, lon);
 }
 
 /** ***************************** filtering markers ************************** */
@@ -106,33 +119,34 @@ function getLatLng(str)
  * clicked point data stored in array (pos[])
  */
 function start() {
-	if (startAllowed == false) {
-		alert("Reset to Start");
-		return;
-	}
-	$('#markertable').empty();
-	listenerHandler = google.maps.event.addListener(map, 'click', function(e) {
-		pos[posIndex] = e.latLng;
-		if (posIndex > 0) {
-			polyline(posIndex - 1, posIndex);
-		}
-		posIndex += 1;
-	});
+  if (startAllowed == false) {
+    alert("Reset to Start");
+    return;
+  }
+  $('#usertable').empty();
+  listenerHandler = google.maps.event.addListener(map, 'click', function(e) {
+    pos[posIndex] = e.latLng;
+    if (posIndex > 0) {
+      polyline(posIndex - 1, posIndex);
+    }
+    posIndex += 1;
+  });
 
 }
+
 /**
  * Stop drawing the sequence of polylines 
  * Update listeners Invoke drawPolygon method
  */
 function stop() {
-	polyline(pos.length - 1, 0); // close the polygon: last to first points
-	// it would be better to somehow convert existing polyline to polygon
-	// but for the moment this will do - overlaying polyline with polygon
-	drawPolygon();
-	google.maps.event.removeListener(listenerHandler);
-	listenerHandler = null;
-	startAllowed = false; // ensures start() invokable once only between
-	// refreshes
+  polyline(pos.length - 1, 0); // close the polygon: last to first points
+  // it would be better to somehow convert existing polyline to polygon
+  // but for the moment this will do - overlaying polyline with polygon
+  drawPolygon();
+  google.maps.event.removeListener(listenerHandler);
+  listenerHandler = null;
+  startAllowed = false; // ensures start() invokable once only between
+  // refreshes
 }
 
 /**
@@ -142,22 +156,22 @@ function stop() {
  * within polyon are rendered and those outside are not displayed
  */
 function filter() {
-	for (var i = 0; i < latlng.length; i += 1) {
-		var point = new google.maps.LatLng(latlng[i][1], latlng[i][2]);
-		if (google.maps.geometry.poly.containsLocation(point, polygon)) {
-			markers[i].setVisible(true);
-			populateTableRow(latlng[i]);
-		} else {
-			markers[i].setVisible(false);
-		}
-	}
+  for (var i = 0; i < latlng.length; i += 1) {
+    var point = new google.maps.LatLng(latlng[i][1], latlng[i][2]);
+    if (google.maps.geometry.poly.containsLocation(point, polygon)) {
+      markers[i].setVisible(true);
+      // populateTableRow(latlng[i]);
+    } else {
+      markers[i].setVisible(false);
+    }
+  }
 }
 
 /**
  * Clears table row data Restores table data with complete unfiltered user list
  */
 function reset() {
-	location.reload();
+  location.reload();
 }
 
 /**
@@ -168,65 +182,43 @@ function reset() {
  * @param index
  */
 function polyline(prevIndex, index) {
-	var coords = [
-	              new google.maps.LatLng(pos[prevIndex].lat(), pos[prevIndex].lng()),
-	              new google.maps.LatLng(pos[index].lat(), pos[index].lng())];
+  var coords = [
+      new google.maps.LatLng(pos[prevIndex].lat(), pos[prevIndex].lng()),
+      new google.maps.LatLng(pos[index].lat(), pos[index].lng())];
 
-	var line = new google.maps.Polyline({
-		path : coords,
-		geodesic : true,
-		strokeColor : '#FF0000',
-		strokeOpacity : 1.0,
-		strokeWeight : 2
-	});
-	line.setMap(map);
+  var line = new google.maps.Polyline({
+    path : coords,
+    geodesic : true,
+    strokeColor : '#FF0000',
+    strokeOpacity : 1.0,
+    strokeWeight : 2
+  });
+  line.setMap(map);
 }
 
 /**
  * Use data (pos[]) to draw polygon
  */
 function drawPolygon() {
-	var lineCoords = [];
-	// log the coordinates
-	// draw polygon defined by coordinates
-	for (var j = 0; j < pos.length; j += 1) {
-		console.log(pos[j].lat + " " + pos[j].lng);
-		lineCoords[j] = new google.maps.LatLng(pos[j].lat(), pos[j].lng());
-	}
-	// make last point same as first to close the polygon
-	lineCoords[pos.length] = new google.maps.LatLng(pos[0].lat(), pos[0].lng());
+  var lineCoords = [];
+  // log the coordinates
+  // draw polygon defined by coordinates
+  for (var j = 0; j < pos.length; j += 1) {
+    console.log(pos[j].lat + " " + pos[j].lng);
+    lineCoords[j] = new google.maps.LatLng(pos[j].lat(), pos[j].lng());
+  }
+  // make last point same as first to close the polygon
+  lineCoords[pos.length] = new google.maps.LatLng(pos[0].lat(), pos[0].lng());
 
-	polygon = new google.maps.Polyline({
-		path : lineCoords,
-		geodesic : true,
-		strokeColor : '#FF0000',
-		strokeOpacity : 1.0,
-		strokeWeight : 2
-	});
+  polygon = new google.maps.Polyline({
+    path : lineCoords,
+    geodesic : true,
+    strokeColor : '#FF0000',
+    strokeOpacity : 1.0,
+    strokeWeight : 2
+  });
 
-	polygon.setMap(map);
-	google.maps.event.clearListeners(map, 'click');
-}
-
-/*******************************populating table with marker data*************************/
-/**
- * Populates table with complete marker list + it's gps coords
- */
-function populateTable()
-{
-	$.each(latlng, function(i, val) {
-		populateTableRow(val);
-	});
-}
-
-/**
- * renders table row comprising marker and its gps coordinates
- * @param data the array comprising description + gps (lat, lng)
- */
-function populateTableRow(data)
-{
-	var description = "<td>" + data[0] + "</td>";
-	var gps   = "<td>" + data[1] + " " + data[2] + "</td>";
-	$('#markertable').append("<tr>" + description + gps + "</tr>");
+  polygon.setMap(map);
+  google.maps.event.clearListeners(map, 'click');
 }
 google.maps.event.addDomListener(window, 'load', initialize);
